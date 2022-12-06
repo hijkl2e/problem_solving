dc3729cbefd8f88135ae5875d2e2a0341bf40664
Hyunjun Lee <hijkl2e@naver.com>
2022-12-07 00:36:12 +0900

    Add solved.ac 새싹 solutions

---
+++ boj/boj1008.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int a{}, b{};
+	cin >> a >> b;
+	cout << fixed << setprecision(10);
+	cout << a * 1.0 / b << "\n";
+	return 0;
+}
