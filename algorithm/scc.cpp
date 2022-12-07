2eded191fca8b5d1d1201c81d58b292715477490
Hyunjun Lee <hijkl2e@naver.com>
2022-12-07 13:02:25 +0900

    Add scc.cpp

---
+++ algorithm/scc.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<int>> G, H;
+stack<int> st;
+vector<int> scc;
+
+void dfs(int x, int scc_num) {
+	scc[x] = scc_num == -1 ? 0 : scc_num;
+	for (int y : scc_num == -1 ? G[x] : H[x]) {
+		if (scc[y] == -1) {
+			dfs(y, scc_num);
+		}
+	}
+	if (scc_num == -1) {
+		st.push(x);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int N{}, M{};
+	cin >> N >> M;
+	G.resize(N + 1);
+	H.resize(N + 1);
+	while (M--) {
+		int u{}, v{};
+		cin >> u >> v;
+		G[u].push_back(v);
+		H[v].push_back(u);
+	}
+	scc.assign(N + 1, -1);
+	for (int i = 1; i <= N; ++i) {
+		if (scc[i] == -1) {
+			dfs(i, -1);
+		}
+	}
+	fill(scc.begin(), scc.end(), -1);
+	int scc_num{};
+	while (st.size()) {
+		int x = st.top(); st.pop();
+		if (scc[x] == -1) {
+			dfs(x, scc_num++);
+		}
+	}
+	return 0;
+}
