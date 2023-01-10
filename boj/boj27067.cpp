9857d2d7e04720b7b273cef2450a7bd049fe72c5
Hyunjun Lee <hijkl2e@naver.com>
2023-01-11 06:11:02 +0900

    Update boj27067.cpp

--- boj/boj27067.cpp
+++ boj/boj27067.cpp
@@ -21,38 +21,11 @@ int main() {
 			}
 		}
 	}
-	vector<vector<int>> G(N + 1);
-	for (int i = 1; i <= N; ++i) {
-		for (int j = 1; j <= N; ++j) {
-			if (B[i][j] > 1) {
-				G[i].push_back(j);
-			}
-		}
-	}
-	vector<int> ind(N + 1);
-	for (int i = 1; i <= N; ++i) {
-		for (int x : G[i]) {
-			++ind[x];
-		}
-	}
-	queue<int> q;
-	for (int i = 1; i <= N; ++i) {
-		if (ind[i] == 0) {
-			q.push(i);
-		}
-	}
-	vector<int> C;
-	while (q.size()) {
-		int x = q.front(); q.pop();
-		for (int y : G[x]) {
-			if (--ind[y] == 0) {
-				q.push(y);
-			}
-		}
-		C.push_back(x);
-	}
-	for (int i = 0; i < C.size(); ++i) {
-		cout << C[i] << (i == C.size() - 1 ? "\n" : " ");
+	sort(A[0].begin(), A[0].end(), [&](int a, int b) {
+		return B[a][b] > 1;
+	});
+	for (int i = 0; i < N; ++i) {
+		cout << A[0][i] << (i == N - 1 ? "\n" : " ");
 	}
 	return 0;
 }
