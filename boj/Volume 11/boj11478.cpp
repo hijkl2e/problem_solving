49ccdc3c4eb68308102547d322feff933322dc73
Hyunjun Lee <hijkl2e@naver.com>
2023-03-07 13:40:11 +0900

    Update boj11478.cpp

--- boj/Volume 11/boj11478.cpp
+++ boj/Volume 11/boj11478.cpp
@@ -2,34 +2,39 @@
 
 using namespace std;
 
-int trie_cnt;
-
 struct Trie {
-	Trie() : next(26) {
-		++trie_cnt;
+	Trie() {
+		memset(next, -1, sizeof next);
 	}
 	void insert(string &s, int i) {
 		if (i == s.size()) {
 			return;
 		}
 		int j = s[i] - 'a';
-		if (next[j] == nullptr) {
-			next[j] = new Trie();
+		if (next[j] == -1) {
+			next[j] = trie_cnt++;
 		}
-		next[j]->insert(s, i + 1);
+		node[next[j]].insert(s, i + 1);
 	}
-	vector<Trie *> next;
+	int next[26]{};
+	static int trie_cnt;
+	static vector<Trie> node;
 };
+
+int Trie::trie_cnt;
+vector<Trie> Trie::node;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	string s;
 	getline(cin, s);
-	Trie *root = new Trie();
+	Trie::trie_cnt = 1;
+	Trie::node.resize(1000001);
+	Trie &root = Trie::node[0];
 	for (int i = 0; i < s.size(); ++i) {
-		root->insert(s, i);
+		root.insert(s, i);
 	}
-	cout << trie_cnt - 1 << "\n";
+	cout << Trie::trie_cnt - 1 << "\n";
 	return 0;
 }
