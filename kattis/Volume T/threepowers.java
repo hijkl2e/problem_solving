34193e3b801c1836b6179f8a34e44c7313bac2da
Hyunjun Lee <hijkl2e@naver.com>
2023-05-06 01:34:51 +0900

    Update threepowers.cpp

--- kattis/Volume T/threepowers.java
+++ kattis/Volume T/threepowers.java
@@ -1,6 +1,6 @@
-import java.math.BigInteger;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class threepowers {
 	public static void main(String[] args) throws Exception {
