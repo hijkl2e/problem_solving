d876dceeca96da04e7530d99a68e4bde78cac2d7
Hyunjun Lee <hijkl2e@naver.com>
2023-05-12 03:39:28 +0900

    Update boj13511.cpp

--- boj/Volume 13/boj13511.cpp
+++ boj/Volume 13/boj13511.cpp
@@ -3,76 +3,54 @@
 using namespace std;
 
 using ll = long long;
+using ii = pair<ll, ll>;
 
-vector<vector<int>> G;
-vector<int> sz, dep, par, top, in, out, rev;
-vector<ll> wt;
+vector<vector<ii>> G;
+vector<int> d;
+vector<vector<int>> par;
+vector<vector<ll>> c;
 
-void dfs1(int u) {
-	sz[u] = 1;
-	for (auto &v : G[u]) {
-		if (par[u] == v) {
+void dfs(int u) {
+	for (auto &[v, w] : G[u]) {
+		if (v == par[0][u]) {
 			continue;
 		}
-		dep[v] = dep[u] + 1;
-		par[v] = u;
-		dfs1(v);
-		sz[u] += sz[v];
-		if (sz[v] > sz[G[u][0]]) {
-			swap(v, G[u][0]);
-		}
+		d[v] = d[u] + 1;
+		par[0][v] = u;
+		c[0][v] = w;
+		dfs(v);
 	}
-}
-
-void dfs2(int u) {
-	for (auto &v : G[u]) {
-		if (par[u] == v) {
-			continue;
-		}
-		in[v] = out[v] = out[u] + 1;
-		top[v] = v == G[u][0] ? top[u] : v;
-		dfs2(v);
-		out[u] = out[v];
-	}
-}
-
-ll solve(int u, int v) {
-	ll ret{};
-	while (top[u] != top[v]) {
-		if (dep[top[u]] > dep[top[v]]) {
-			swap(u, v);
-		}
-		ret += wt[in[v]] - wt[in[top[v]] - 1];
-		v = par[top[v]];
-	}
-	if (dep[u] > dep[v]) {
-		swap(u, v);
-	}
-	ret += wt[in[v]] - wt[in[u]];
-	return ret;
 }
 
 int lca(int u, int v) {
-	while (top[u] != top[v]) {
-		if (dep[top[u]] > dep[top[v]]) {
-			swap(u, v);
-		}
-		v = par[top[v]];
+	if (d[u] > d[v]) {
+		swap(u, v);
 	}
-	return dep[u] < dep[v] ? u : v;
+	while (d[v] - d[u]) {
+		int x = __builtin_ctz(d[v] - d[u]);
+		v = par[x][v];
+	}
+	if (u == v) {
+		return u;
+	}
+	for (int i = 16; i >= 0; --i) {
+		if (par[i][u] != par[i][v]) {
+			u = par[i][u];
+			v = par[i][v];
+		}
+	}
+	return par[0][u];
 }
 
-int solve(int u, int v, int k) {
-	int l = lca(u, v);
-	if (dep[u] - dep[l] < k) {
-		k = dep[u] + dep[v] - 2 * dep[l] - k;
-		u = v;
+ii solve(int u, int s) {
+	ll ret{};
+	while (s) {
+		int x = __builtin_ctz(s);
+		ret += c[x][u];
+		u = par[x][u];
+		s &= ~(1 << x);
 	}
-	while (k >= dep[u] - dep[top[u]] + 1) {
-		k -= dep[u] - dep[top[u]] + 1;
-		u = par[top[u]];
-	}
-	return rev[in[u] - k];
+	return {u, ret};
 }
 
 int main() {
@@ -81,48 +59,40 @@ int main() {
 	int N{};
 	cin >> N;
 	G.resize(N + 1);
-	vector<int> u(N);
-	vector<int> v(N);
-	vector<int> w(N);
-	for (int i = 1; i < N; ++i) {
-		cin >> u[i] >> v[i] >> w[i];
-		G[u[i]].push_back(v[i]);
-		G[v[i]].push_back(u[i]);
+	for (int i = 0; i < N - 1; ++i) {
+		int u{}, v{}, w{};
+		cin >> u >> v >> w;
+		G[u].push_back({v, w});
+		G[v].push_back({u, w});
 	}
-	sz.resize(N + 1);
-	dep.resize(N + 1);
-	par.resize(N + 1);
-	top.resize(N + 1);
-	in.resize(N + 1);
-	out.resize(N + 1);
-	par[1] = -1;
-	top[1] = 1;
-	in[1] = out[1] = 1;
-	dfs1(1);
-	dfs2(1);
-	rev.resize(N + 1);
-	for (int i = 1; i <= N; ++i) {
-		rev[in[i]] = i;
-	}
-	wt.resize(N + 1);
-	for (int i = 1; i < N; ++i) {
-		if (dep[u[i]] > dep[v[i]]) {
-			swap(u[i], v[i]);
+	d.resize(N + 1);
+	par.assign(17, vector<int>(N + 1));
+	c.assign(17, vector<ll>(N + 1));
+	dfs(1);
+	for (int i = 1; i < 17; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			par[i][j] = par[i - 1][par[i - 1][j]];
+			c[i][j] = c[i - 1][j] + c[i - 1][par[i - 1][j]];
 		}
-		wt[in[v[i]]] = w[i];
 	}
-	partial_sum(wt.begin(), wt.end(), wt.begin());
 	int M{};
 	cin >> M;
 	while (M--) {
-		int q{}, a{}, b{}, c{};
-		cin >> q >> a >> b;
+		int q{}, u{}, v{}, k{};
+		cin >> q >> u >> v;
+		int l = lca(u, v);
+		ll ans{};
 		if (q == 1) {
-			cout << solve(a, b) << "\n";
+			ans = solve(u, d[u] - d[l]).second + solve(v, d[v] - d[l]).second;
 		} else {
-			cin >> c;
-			cout << solve(a, b, c - 1) << "\n";
+			cin >> k;
+			if (k > d[u] - d[l]) {
+				k = d[u] + d[v] - 2 * d[l] - k + 2;
+				swap(u, v);
+			}
+			ans = solve(u, k - 1).first;
 		}
+		cout << ans << "\n";
 	}
 	return 0;
 }
