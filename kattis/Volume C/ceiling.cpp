be62e42cc1728b42893155d4aad9d6c1001c48ec
Hyunjun Lee <hijkl2e@naver.com>
2023-05-25 17:31:19 +0900

    Update ceiling.cpp

--- kattis/Volume C/ceiling.cpp
+++ kattis/Volume C/ceiling.cpp
@@ -7,24 +7,25 @@ int main() {
 	cin.tie(nullptr);
 	int N{}, K{};
 	cin >> N >> K;
-	vector<int> A(1 << (K + 1));
-	vector<vector<int>> B(N);
+	vector<map<int, int>> A(N);
 	for (int i = 0; i < N; ++i) {
-		fill(A.begin(), A.end(), 0);
 		for (int j = 0; j < K; ++j) {
 			int x{};
 			cin >> x;
 			int p = 1;
-			while (A[p]) {
-				p = 2 * p + (A[p] < x);
+			while (A[i][p]) {
+				p = 2 * p + (x > A[i][p]);
 			}
-			A[p] = x;
-			B[i].push_back(p);
+			A[i][p] = x;
 		}
-		sort(B[i].begin(), B[i].end());
 	}
-	sort(B.begin(), B.end());
-	B.resize(unique(B.begin(), B.end()) - B.begin());
-	cout << B.size() << "\n";
+	for (int i = 0; i < N; ++i) {
+		for (auto &p : A[i]) {
+			p.second = 0;
+		}
+	}
+	sort(A.begin(), A.end());
+	A.resize(unique(A.begin(), A.end()) - A.begin());
+	cout << A.size() << "\n";
 	return 0;
 }
